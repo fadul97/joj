801a4ba6e13897d5297a42ee7b064ff7336cdd93
Leonardo Fadul <lsffadul@gmail.com>
2024-12-07 20:09:00 -0300

    Library jojPlatform

---
+++ joj/include/joj/platform/window.h
@@ -0,0 +1,9 @@
+#ifndef _JOJ_WINDOW_H
+#define _JOJ_WINDOW_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+JAPI void window_print();
+
+#endif // _JOJ_WINDOW_H
---
+++ joj/src/platform/window.cpp
@@ -0,0 +1,8 @@
+#include "platform/window.h"
+
+#include <iostream>
+
+void window_print()
+{
+    std::cout << "Hello from Window!" << std::endl;
+}
