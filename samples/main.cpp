5ee997d671a84b0785d3481651e3eeaa07330cb4
Leonardo Fadul <lsffadul@gmail.com>
2024-12-09 23:10:57 -0300

    Removed window code in main

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,16 +1,12 @@
-#include "joj/platform/window.h"
 #include <iostream>
 #include "joj/logger.h"
 
 int main()
 {
-    joj::Window window{ "My Window Title", 800, 600 };
-    std::cout << "Window title: " << window.get_title() << std::endl;
     JINFO("Working!");
     JDEBUG("Working!");
     JWARN("Working!");
     JERROR(joj::ErrorCode::FAILED, "Working!");
     JFATAL(joj::ErrorCode::FAILED, "Working!");
-    window_print();
     return 0;
 }
