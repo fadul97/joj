b8678d336f0f1bbc3ec707aba1d561363b0f7b31
Leonardo Fadul <lsffadul@gmail.com>
2024-12-10 00:19:19 -0300

    Enum ErrorCode and helper functions

---
+++ joj/include/joj/error_code.h
@@ -0,0 +1,147 @@
+/**
+ * @file error_code.h
+ * @author Leonardo Fadul ( TODO: todo@domain.com)
+ * @brief This file contains the error codes used throughout the engine,
+ * and some helper functions for debug purposes.
+ * @version 0.1
+ * @date 2024-11-13
+ * 
+ * @copyright TODO: Copyright (c) 2024
+ * 
+ * REFERENCES: Travis Vroman (https://travisvroman.com/) - Kohi Game Engine (https://github.com/travisvroman/kohi)
+ * 
+ */
+
+#ifndef JOJ_ERROR_CODE_H
+#define JOJ_ERROR_CODE_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+namespace joj
+{
+    /** @brief Represents the error codes for the engine. */
+    enum class ErrorCode
+    {
+        /** @brief OK code, should be used when the operation was successful. */
+        OK,
+
+        /** @brief Generic error code, should be used when the operation failed. */
+        FAILED,
+
+        /** @brief Window Error code, should be used for invalid window handle. */
+        ERR_WINDOW_HANDLE,
+
+        /** @brief Window Error code, should be used for invalid window registration. */
+        ERR_WINDOW_REGISTRATION,
+
+        /** @brief Window Error code, should be used when window fails to adjust. */
+        ERR_WINDOW_ADJUST,
+
+        /** @brief Window Error code, should be used when window fails to move. */
+        ERR_WINDOW_MOVE,
+
+        /** @brief Window Error code, should be used when the device context fails to create. */
+        ERR_WINDOW_DEVICE_CONTEXT,
+
+        /** @brief Window Error code, should be used when the window fails to get its rect. */
+        ERR_WINDOW_RECT,
+
+        /** @brief Window Error code, should be used when the window fails to get its client rect. */
+        ERR_WINDOW_CLIENT_RECT,
+
+        /** @brief D3D11 Context Error code, should be used when creation of DXGIFactory2 fails. */
+        ERR_CONTEXT_D3D11_DXGI_FACTORY2_CREATION,
+
+        /** @brief D3D11 Context Error code, should be used when creation of D3D11Device fails. */
+        ERR_CONTEXT_D3D11_DEVICE_CREATION,
+
+        /** @brief D3D11 Context Error code, should be used when creation of Warp Adapter fails. */
+        ERR_CONTEXT_D3D11_WARP_ADAPTER_CREATION,
+
+        /** @brief D3D11 Context Error code, should be used when querying ID3D11Debug fails. */
+        ERR_CONTEXT_D3D11_QUERY_INTERFACE_ID3D11_DEBUG,
+
+        /** @brief D3D11 Context Error code, should be used when querying IDXGIDevice fails. */
+        ERR_CONTEXT_D3D11_QUERY_INTERFACE_IDXGI_DEVICE,
+
+        /** @brief D3D11 Context Error code, should be used when getting parent of IDXGIAdapter fails. */
+        ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_ADAPTER,
+
+        /** @brief D3D11 Context Error code, should be used when getting parent of IDXGIFactory fails. */
+        ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_FACTORY,
+
+        /** @brief D3D11 Context Error code, should be used when creation of D3D11Context fails. */
+        ERR_D3D11CONTEXT_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of SwapChain fails. */
+        ERR_SWAPCHAIN_D311_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when getting back buffer of SwapChain fails. */
+        ERR_SWAPCHAIN_D3D11_GET_BACKBUFFER,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of RenderTargetView fails. */
+        ERR_RENDER_TARGET_VIEW_D3D11_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of DepthStencil Texture fails. */
+        ERR_DEPTHSTENCIL_D3D11_BUFFER,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of DepthStencilView fails. */
+        ERR_DEPTHSTENCIL_VIEW_D3D11_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of BlendState fails. */
+        ERR_BLENDSTATE_D3D11_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when creation of RasterizerState fails. */
+        ERR_RASTERIZER_D3D11_CREATION,
+
+        /** @brief D3D11 Renderer Error code, should be used when SwapChain fails to present. */
+        ERR_RENDERER_D3D11_SWAPCHAIN_PRESENT,
+
+        /** @brief D3D11 Shader Error code, should be used when compilation of Vertex Shader fails. */
+        ERR_SHADER_D3D11_VERTEX_COMPILATION,
+
+        /** @brief D3D11 Shader Error code, should be used when compilation of Pixel Shader fails. */
+        ERR_SHADER_D3D11_PIXEL_COMPILATION,
+
+        /** @brief D3D11 Shader Error code, should be used when compilation of Geometry Shader fails. */
+        ERR_SHADER_D3D11_GEOMETRY_COMPILATION,
+
+        /** @brief D3D11 Shader Error code, should be used when compilation of Compute Shader fails. */
+        ERR_SHADER_D3D11_COMPUTE_COMPILATION,
+
+        /** @brief D3D11 Renderer code, should be used when D3D11Renderer fails to initialize. */
+        ERR_RENDERER_D3D11_INIT,
+
+        ERR_RENDER_STATE_D3D11_RASTERIZER_STATE_CREATION,
+        ERR_RENDER_STATE_D3D11_BLEND_STATE_CREATION,
+        ERR_RENDER_STATE_D3D11_DEPTHSTENCIL_STATE_CREATION,
+        ERR_RENDERER_D3D11_TEXTURE2D_CREATION,
+        ERR_RENDERER_D3D11_SHADER_RESOURCE_VIEW_CREATION,
+        ERR_RENDERER_D3D11_UNORDERED_ACCESS_VIEW_CREATION,
+        ERR_RENDERER_D3D11_VERTEX_BUFFER_CREATION,
+        ERR_RENDERER_D3D11_INDEX_BUFFER_CREATION,
+        ERR_RENDERER_D3D11_CONSTANT_BUFFER_CREATION,
+
+        ERR_SHADER_D3D11_VERTEX_CREATION,
+        ERR_SHADER_D3D11_PIXEL_CREATION,
+        ERR_SAMPLER_STATE_D3D11_CREATION,
+        ERR_D3D11_INPUT_LAYOUT_CREATION,
+    };
+
+    // TODO: Add all cases.
+    /** @brief Get the index of the ErrorCode.
+     *  @param err The error code to convert.
+     *  @return The integer index of the error code.
+     */
+    i32 err_to_int(ErrorCode err);
+
+    // TODO: Add all cases.
+    /** @brief Get the name of the ErrorCode.
+     *  @param err The error code to convert.
+     *  @return The string representation of the error code.
+     */
+    const char* err_to_str(ErrorCode err);
+}
+
+#endif // JOJ_ERROR_CODE_H
---
+++ joj/src/error_code.cpp
@@ -0,0 +1,187 @@
+#include "error_code.h"
+
+int joj::err_to_int(const ErrorCode err)
+{
+    switch (err)
+    {
+    case ErrorCode::OK:
+        return 0;
+
+    case ErrorCode::FAILED:
+        return -1;
+
+    case ErrorCode::ERR_WINDOW_HANDLE:
+        return -2;
+
+    case ErrorCode::ERR_WINDOW_REGISTRATION:
+        return -3;
+
+    case ErrorCode::ERR_WINDOW_ADJUST:
+        return -4;
+
+    case ErrorCode::ERR_WINDOW_MOVE:
+        return -5;
+
+    case ErrorCode::ERR_WINDOW_DEVICE_CONTEXT:
+        return -6;
+
+    case ErrorCode::ERR_WINDOW_RECT:
+        return -7;
+
+    case ErrorCode::ERR_WINDOW_CLIENT_RECT:
+        return -8;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_DXGI_FACTORY2_CREATION:
+        return -9;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_DEVICE_CREATION:
+        return -10;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_WARP_ADAPTER_CREATION:
+        return -11;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_QUERY_INTERFACE_ID3D11_DEBUG:
+        return -12;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_QUERY_INTERFACE_IDXGI_DEVICE:
+        return -13;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_ADAPTER:
+        return -14;
+
+    case ErrorCode::ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_FACTORY:
+        return -15;
+
+    case ErrorCode::ERR_D3D11CONTEXT_CREATION:
+        return -16;
+
+    case ErrorCode::ERR_SWAPCHAIN_D311_CREATION:
+        return -17;
+
+    case ErrorCode::ERR_SWAPCHAIN_D3D11_GET_BACKBUFFER:
+        return -18;
+
+    case ErrorCode::ERR_RENDER_TARGET_VIEW_D3D11_CREATION:
+        return -19;
+
+    case ErrorCode::ERR_DEPTHSTENCIL_D3D11_BUFFER:
+        return -20;
+
+    case ErrorCode::ERR_DEPTHSTENCIL_VIEW_D3D11_CREATION:
+        return -21;
+
+    case ErrorCode::ERR_BLENDSTATE_D3D11_CREATION:
+        return -22;
+
+    case ErrorCode::ERR_RASTERIZER_D3D11_CREATION:
+        return -23;
+
+    case ErrorCode::ERR_RENDERER_D3D11_SWAPCHAIN_PRESENT:
+        return -24;
+
+    case ErrorCode::ERR_SHADER_D3D11_VERTEX_COMPILATION:
+        return -25;
+
+    case ErrorCode::ERR_SHADER_D3D11_PIXEL_COMPILATION:
+        return -26;
+
+    case ErrorCode::ERR_RENDERER_D3D11_INIT:
+        return -27;
+
+    default:
+        return -1;
+    }
+}
+
+const char* joj::err_to_str(const ErrorCode err)
+{
+    switch (err)
+    {
+    case ErrorCode::OK:
+        return "OK";
+
+    case ErrorCode::FAILED:
+        return "FAILED";
+
+    case ErrorCode::ERR_WINDOW_HANDLE:
+        return "ERR_WINDOW_HANDLE";
+
+    case ErrorCode::ERR_WINDOW_REGISTRATION:
+        return "ERR_WINDOW_REGISTRATION";
+
+    case ErrorCode::ERR_WINDOW_ADJUST:
+        return "ERR_WINDOW_ADJUST";
+
+    case ErrorCode::ERR_WINDOW_MOVE:
+        return "ERR_WINDOW_MOVE";
+
+    case ErrorCode::ERR_WINDOW_DEVICE_CONTEXT:
+        return "ERR_WINDOW_DEVICE_CONTEXT";
+
+    case ErrorCode::ERR_WINDOW_RECT:
+        return "ERR_WINDOW_RECT";
+
+    case ErrorCode::ERR_WINDOW_CLIENT_RECT:
+        return "ERR_WINDOW_CLIENT_RECT";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_DXGI_FACTORY2_CREATION:
+        return "ERR_CONTEXT_D3D11_DXGI_FACTORY2_CREATION";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_DEVICE_CREATION:
+        return "ERR_CONTEXT_D3D11_DEVICE_CREATION";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_WARP_ADAPTER_CREATION:
+        return "ERR_CONTEXT_D3D11_WARP_ADAPTER_CREATION";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_QUERY_INTERFACE_ID3D11_DEBUG:
+        return "ERR_CONTEXT_D3D11_QUERY_INTERFACE_ID3D11_DEBUG";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_QUERY_INTERFACE_IDXGI_DEVICE:
+        return "ERR_CONTEXT_D3D11_QUERY_INTERFACE_IDXGI_DEVICE";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_ADAPTER:
+        return "ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_ADAPTER";
+
+    case ErrorCode::ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_FACTORY:
+        return "ERR_CONTEXT_D3D11_GET_PARENTOF_IDXGI_FACTORY";
+
+    case ErrorCode::ERR_D3D11CONTEXT_CREATION:
+        return "ERR_D3D11CONTEXT_CREATION";
+
+    case ErrorCode::ERR_SWAPCHAIN_D311_CREATION:
+        return "ERR_SWAPCHAIN_D311_CREATION";
+
+    case ErrorCode::ERR_SWAPCHAIN_D3D11_GET_BACKBUFFER:
+        return "ERR_SWAPCHAIN_D3D11_GET_BACKBUFFER";
+
+    case ErrorCode::ERR_RENDER_TARGET_VIEW_D3D11_CREATION:
+        return "ERR_RENDER_TARGET_VIEW_D3D11_CREATION";
+
+    case ErrorCode::ERR_DEPTHSTENCIL_D3D11_BUFFER:
+        return "ERR_DEPTHSTENCIL_D3D11_BUFFER";
+
+    case ErrorCode::ERR_DEPTHSTENCIL_VIEW_D3D11_CREATION:
+        return "ERR_DEPTHSTENCIL_VIEW_D3D11_CREATION";
+
+    case ErrorCode::ERR_BLENDSTATE_D3D11_CREATION:
+        return "ERR_BLENDSTATE_D3D11_CREATION";
+
+    case ErrorCode::ERR_RASTERIZER_D3D11_CREATION:
+        return "ERR_RASTERIZER_D3D11_CREATION";
+
+    case ErrorCode::ERR_RENDERER_D3D11_SWAPCHAIN_PRESENT:
+        return "ERR_RENDERER_D3D11_SWAPCHAIN_PRESENT";
+
+    case ErrorCode::ERR_SHADER_D3D11_VERTEX_COMPILATION:
+        return "ERR_SHADER_D3D11_VERTEX_COMPILATION";
+
+    case ErrorCode::ERR_SHADER_D3D11_PIXEL_COMPILATION:
+        return "ERR_SHADER_D3D11_PIXEL_COMPILATION";
+
+    case ErrorCode::ERR_RENDERER_D3D11_INIT:
+        return "ERR_RENDERER_D3D11_INIT";
+
+    default:
+        return "UNKNOWN";
+    }
+}
