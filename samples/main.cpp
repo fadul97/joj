b14a27dc014f8138e22a9ac645201bc98e63776a
Leonardo Fadul <lsffadul@gmail.com>
2024-12-11 20:01:02 -0300

    Added jojWin32Platform library to joj and using it in main

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "joj/logger.h"
+#include "joj/platform/win32/window_win32.h"
 
 int main()
 {
-    JINFO("Working!");
-    JDEBUG("Working!");
-    JWARN("Working!");
-    JERROR(joj::ErrorCode::FAILED, "Working!");
-    JFATAL(joj::ErrorCode::FAILED, "Working!");
+    joj::Win32Window window{ "jojWindow", 800, 600, joj::WindowMode::Windowed };
+    if (window.create() != joj::ErrorCode::OK)
+    {
+        JFATAL(joj::ErrorCode::FAILED, "Failed to create Win32Window");
+        return -1;
+    }
+
+    MSG msg = { 0 };
+    bool loop = true;
+    while (loop)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
+                loop = false;
+        }
+    }
+
     return 0;
 }
