c1aa73ab1ac6590ca21a69417476ba8348f4aa01
Leonardo Fadul <lsffadul@gmail.com>
2024-12-11 17:42:15 -0300

    Testing input class in main

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "joj/logger.h"
 #include "joj/platform/win32/window_win32.h"
+#include "joj/platform/win32/input_win32.h"
 
 int main()
 {
@@ -20,6 +21,9 @@ int main()
     window.get_client_size(width, height);
     JDEBUG("Client size: %dx%d", width, height);
 
+    joj::Win32Input input{};
+    input.set_window(window.get_data());
+
     MSG msg = { 0 };
     bool loop = true;
     while (loop)
@@ -32,6 +36,15 @@ int main()
             if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
                 loop = false;
         }
+
+        if (input.is_key_pressed('A'))
+            JDEBUG("A pressed.");
+
+        if (input.is_key_down(joj::KEY_SPACE))
+            JDEBUG("Space down.");
+
+        if (input.is_key_pressed(joj::KEY_ESCAPE))
+            loop = false;
     }
 
     return 0;
