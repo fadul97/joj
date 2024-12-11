02918c14661a76795c3f8a2d82f0d2f8c9fb449f
Leonardo Fadul <lsffadul@gmail.com>
2024-12-11 17:41:02 -0300

    Input base class

---
+++ joj/include/joj/platform/input.h
@@ -0,0 +1,64 @@
+#ifndef _JOJ_INPUT_H
+#define _JOJ_INPUT_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "keys.h"
+#include "window.h"
+
+namespace joj
+{
+    struct InputData
+    {
+        Keyboard keyboard;
+        Keyboard ctrl;
+        Mouse mouse;
+    };
+
+    class JAPI Input
+    {
+    public:
+        Input();
+        virtual ~Input();
+
+        virtual void set_window(WindowData window) = 0;
+
+        b8 is_key_down(u32 key);
+        b8 is_key_pressed(u32 key);
+        b8 is_key_up(u32 key);
+
+        b8 is_button_down(Buttons button) const;
+        b8 is_button_up(Buttons button) const;
+
+        i16 get_xmouse();
+        i16 get_ymouse();
+        i16 get_mouse_wheel();
+
+    protected:
+        static InputData m_data;
+    };
+
+    inline b8 Input::is_key_down(const u32 key)
+    { return m_data.keyboard.keys[key]; }
+
+    inline b8 Input::is_key_up(const u32 key)
+    { return !m_data.keyboard.keys[key]; }
+
+    inline b8 Input::is_button_down(Buttons button) const
+    { return m_data.mouse.buttons[button]; }
+
+    inline b8 Input::is_button_up(Buttons button) const
+    { return !m_data.mouse.buttons[button]; }
+
+    inline i16 Input::get_xmouse()
+    { return m_data.mouse.x; }
+
+    inline i16 Input::get_ymouse()
+    { return m_data.mouse.y; }
+
+    inline i16 Input::get_mouse_wheel()
+    { return m_data.mouse.wheel; }
+}
+
+#endif // _JOJ_INPUT_H
---
+++ joj/src/platform/input.cpp
@@ -0,0 +1,29 @@
+#include "platform/input.h"
+
+joj::InputData joj::Input::m_data{};
+
+joj::Input::Input()
+{
+}
+
+joj::Input::~Input()
+{
+}
+
+b8 joj::Input::is_key_pressed(const u32 key)
+{
+    if (m_data.ctrl.keys[key])
+    {
+        if (is_key_down(key))
+        {
+            m_data.ctrl.keys[key] = false;
+            return true;
+        }
+    }
+    else if (is_key_up(key))
+    {
+        m_data.ctrl.keys[key] = true;
+    }
+
+    return false;
+}
