8bc85fc929390fd52cf3fbcee1672542877aebb0
Leonardo Fadul <lsffadul@gmail.com>
2024-12-11 18:19:20 -0300

    Setting up renderer folders

---
+++ joj/include/joj/renderer/renderer.h
@@ -0,0 +1,9 @@
+#ifndef _JOJ_RENDERER_H
+#define _JOJ_RENDERER_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+JAPI void renderer_print();
+
+#endif // _JOJ_RENDERER_H
---
+++ joj/src/renderer/renderer.cpp
@@ -0,0 +1,8 @@
+#include "renderer/renderer.h"
+
+#include <iostream>
+
+void renderer_print()
+{
+    std::cout << "Hello from renderer!" << std::endl;
+}
