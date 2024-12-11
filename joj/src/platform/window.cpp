8813a60f7064cb2d09db7d9f81c39d152270570f
Leonardo Fadul <lsffadul@gmail.com>
2024-12-11 19:59:51 -0300

    Source file for Window class - won't compile

--- joj/src/platform/window.cpp
+++ joj/src/platform/window.cpp
@@ -9,9 +9,11 @@ void window_print()
 
 template <typename T>
 joj::Window<T>::Window()
-    : m_width{400}, m_height{400}, m_title{"Window"},
-    m_xpos{0}, m_ypos{0}
+    : m_width{0}, m_height{0}, m_title{""},
+    m_xpos{0}, m_ypos{0}, m_mode{WindowMode::Windowed}
 {
+    m_data = { 0 };
+
     m_window_rect = WindowRect
     {
         .left = 0,
@@ -30,10 +32,13 @@ joj::Window<T>::Window()
 }
 
 template <typename T>
-joj::Window<T>::Window(const char* title, const u32 width, const u32 height)
+joj::Window<T>::Window(const char* title, const u32 width, const u32 height,
+    const WindowMode mode)
     : m_width{width}, m_height{height}, m_title{title},
-    m_xpos{0}, m_ypos{0}
+    m_xpos{0}, m_ypos{0}, m_mode{mode}
 {
+    m_data = { 0 };
+
     m_window_rect = WindowRect
     {
         .left = 0,
@@ -55,3 +60,12 @@ template <typename T>
 joj::Window<T>::~Window()
 {
 }
+
+/*
+ * FIXME: Hacky way.
+ * Maybe put everything in a header file(?)
+ */
+#if JPLATFORM_WINDOWS
+#include "platform/win32/window_win32.h"
+template class joj::Window<joj::WindowData>;
+#endif // JPLATFORM_WINDOWS
