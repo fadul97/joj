4c40ff2cb50578c9f58b2986cad12bce8eeca3fc
Leonardo Fadul <lsffadul@gmail.com>
2024-12-12 09:54:53 -0300

    IRenderer base interface

--- joj/include/joj/renderer/renderer.h
+++ joj/include/joj/renderer/renderer.h
@@ -4,6 +4,34 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
+#include "error_code.h"
+
 JAPI void renderer_print();
+
+namespace joj
+{
+    struct RendererData;
+
+    class JAPI IRenderer
+    {
+    public:
+        IRenderer();
+        virtual ~IRenderer();
+
+        virtual ErrorCode create_context() = 0;
+        virtual void destroy_context() = 0;
+
+        virtual ErrorCode initialize() = 0;
+        virtual void shutdown() = 0;
+
+        virtual void resize(i32 width, i32 height) = 0;
+
+        virtual RendererData& get_data() = 0;
+    };
+
+    inline IRenderer::IRenderer() {}
+
+    inline IRenderer::~IRenderer() {}
+}
 
 #endif // _JOJ_RENDERER_H
