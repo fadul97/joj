8637b36f743248b92ae47a7153fb9ec049ef6755
Leonardo Fadul <lsffadul@gmail.com>
2024-12-12 20:46:31 -0300

    Removed unecessary code

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,79 +1,41 @@
-#include <iostream>
-#include "joj/logger.h"
 #include "joj/platform/win32/window_win32.h"
 #include "joj/platform/win32/input_win32.h"
 #include "joj/platform/win32/timer_win32.h"
 #include <sstream>
 #include "joj/renderer/d3d11/renderer_d3d11.h"
+#include "logger.h"
 
 joj::Win32Window window;
 joj::Win32Input input;
 joj::Win32Timer timer;
+joj::D3D11Renderer renderer;
+b8 loop = true;
 f32 frametime = 0.0f;
 f32 get_frametime();
+b8 initialize_everything();
+void shutdown_everything();
+b8 process_events();
+void update();
+void draw();
 
 int main()
 {
-    window = joj::Win32Window{ "jojWindow", 800, 600, joj::WindowMode::Windowed };
-    if (window.create() != joj::ErrorCode::OK)
-    {
-        JFATAL(joj::ErrorCode::FAILED, "Failed to create Win32Window");
-        return -1;
-    }
+    if (!initialize_everything())
+        return 1;
 
-    u32 width = 0;
-    u32 height = 0;
-
-    window.get_window_size(width, height);
-    JDEBUG("Window size: %dx%d", width, height);
-
-    window.get_client_size(width, height);
-    JDEBUG("Client size: %dx%d", width, height);
-
-    input.set_window(window.get_data());
-
-    timer.begin_period();
-
-    MSG msg = { 0 };
-    bool loop = true;
-
-    renderer_print();
-
-    joj::D3D11Renderer renderer;
-    if (renderer.initialize(window.get_data()) != joj::ErrorCode::OK)
-    {
-        JFATAL(joj::ErrorCode::FAILED, "Failed to create D3D11 Context");
-        return -2;
-    }
-
-    timer.start();
     while (loop)
     {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-
-            if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
-                loop = false;
-        }
+        if (!process_events())
+            loop = false;
 
         frametime = get_frametime();
 
-        if (input.is_key_pressed('A'))
-            JDEBUG("A pressed.");
+        update();
 
-        if (input.is_key_down(joj::KEY_SPACE))
-            JDEBUG("Space down.");
-
-        if (input.is_key_pressed(joj::KEY_ESCAPE))
-            loop = false;
-
-        renderer.clear(1.0f, 1.0f, 0.0f, 1.0f);
-        renderer.swap_buffers();
+        draw();
     }
 
-    timer.end_period();
+    shutdown_everything();
 
     return 0;
 }
@@ -114,4 +76,71 @@ f32 get_frametime()
 #endif // JOJ_DEBUG_MODE
 
     return frametime;
+}
+
+b8 initialize_everything()
+{
+    window = joj::Win32Window{ "jojWindow", 800, 600, joj::WindowMode::Windowed };
+    if (window.create() != joj::ErrorCode::OK)
+        return false;
+
+    u32 width = 0;
+    u32 height = 0;
+
+    window.get_window_size(width, height);
+    JDEBUG("Window size: %dx%d", width, height);
+
+    window.get_client_size(width, height);
+    JDEBUG("Client size: %dx%d", width, height);
+
+    input.set_window(window.get_data());
+
+    timer.begin_period();
+
+    renderer_print();
+
+    if (renderer.initialize(window.get_data()) != joj::ErrorCode::OK)
+        return false;
+
+    timer.start();
+
+    return true;
+}
+
+void shutdown_everything()
+{
+    timer.end_period();
+}
+
+b8 process_events()
+{
+    MSG msg = { 0 };
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+        if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
+            return false;
+    }
+
+    return true;
+}
+
+void update()
+{
+    if (input.is_key_pressed('A'))
+        JDEBUG("A pressed.");
+
+    if (input.is_key_down(joj::KEY_SPACE))
+        JDEBUG("Space down.");
+
+    if (input.is_key_pressed(joj::KEY_ESCAPE))
+        loop = false;
+}
+
+void draw()
+{
+    renderer.clear(1.0f, 1.0f, 0.0f, 1.0f);
+    renderer.swap_buffers();
 }
