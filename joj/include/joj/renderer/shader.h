40839d57b0160f4ee706f01928aec0f1c1181af9
Leonardo Fadul <lsffadul@gmail.com>
2024-12-13 23:31:19 -0300

    Renamed methods and 2 new methods to compile shaders from memory

--- joj/include/joj/renderer/shader.h
+++ joj/include/joj/renderer/shader.h
@@ -29,20 +29,24 @@ namespace joj
         Shader();
         virtual ~Shader();
 
-        // Compile from file
-        virtual void compile_vertex_shader(const std::string& vertex_path,
-            const std::string& entry_point, const ShaderModel shader_model) = 0;
-        
-        // Compile from file
-        virtual void compile_pixel_shader(const std::string& pixel_path,
-            const std::string& entry_point, const ShaderModel shader_model) = 0;
-        
-        // Compile from file
-        virtual void compile_geometry_shader(const std::string& pixel_path,
+        // Compile from memory
+        virtual void compile_vertex_shader(const std::string& vertex_shader,
             const std::string& entry_point, const ShaderModel shader_model) = 0;
 
-        // Compile from file
-        virtual void compile_compute_shader(const std::string& pixel_path,
+        // Compile from memory
+        virtual void compile_pixel_shader(const std::string& pixel_shader,
+            const std::string& entry_point, const ShaderModel shader_model) = 0;
+
+        virtual void compile_vertex_shader_from_file(const std::string& vertex_path,
+            const std::string& entry_point, const ShaderModel shader_model) = 0;
+        
+        virtual void compile_pixel_shader_from_file(const std::string& pixel_path,
+            const std::string& entry_point, const ShaderModel shader_model) = 0;
+        
+        virtual void compile_geometry_shader_from_file(const std::string& geometry_path,
+            const std::string& entry_point, const ShaderModel shader_model) = 0;
+
+        virtual void compile_compute_shader_from_file(const std::string& compute_path,
             const std::string& entry_point, const ShaderModel shader_model) = 0;
 
         virtual ErrorCode create_vertex_shader(GraphicsDevice& device) = 0;
