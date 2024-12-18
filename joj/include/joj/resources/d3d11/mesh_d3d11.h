be002061499edcbd54d43b0287dd42fed30d2af5
Leonardo Fadul <lsffadul@gmail.com>
2024-12-18 20:12:06 -0300

    D3D11Mesh class, jojResources and jojD3D11Resources libraries

---
+++ joj/include/joj/resources/d3d11/mesh_d3d11.h
@@ -0,0 +1,47 @@
+#ifndef _JOJ_D3D11_MESH_H
+#define _JOJ_D3D11_MESH_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "resources/mesh.h"
+#include "renderer/d3d11/vertex_buffer_d3d11.h"
+#include "renderer/d3d11/index_buffer_d3d11.h"
+#include "renderer/d3d11/constant_buffer_d3d11.h"
+#include "renderer/d3d11/shader_d3d11.h"
+
+namespace joj
+{
+    class JAPI D3D11Mesh : public Mesh
+    {
+    public:
+        D3D11Mesh();
+        D3D11Mesh(const std::string& filename, MeshType type);
+        ~D3D11Mesh();
+
+        Shader& get_shader() override;
+
+        void setup(GraphicsDevice& device) override;
+
+        void update(const JFloat4x4 view, const JFloat4x4 proj, const f32 dt) override;
+
+        void draw(GraphicsDevice& device, CommandList& cmd_list) override;
+
+    private:
+        D3D11VertexBuffer m_vb;
+        D3D11IndexBuffer m_ib;
+        D3D11ConstantBuffer m_cb;
+        D3D11Shader m_shader;
+        ConstantBuffer m_cb_data;
+
+        b8 load_OBJ(const std::string& filename, std::vector<Vertex>& vertices,
+            std::vector<u32>& indices, u32& vertex_count, u32& index_count);
+    };
+
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // _JOJ_D3D11_MESH_H
