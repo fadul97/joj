77b18872a3da590744f006787a0a524c144bd39e
Leonardo Fadul <lsffadul@gmail.com>
2024-12-22 13:48:56 -0300

    SubMesh struct in jojResources

---
+++ joj/include/joj/resources/submesh.h
@@ -0,0 +1,26 @@
+#ifndef _JOJ_SUBMESH_H
+#define _JOJ_SUBMESH_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include <string>
+
+namespace joj
+{
+	struct SubMesh
+	{
+		SubMesh();
+		SubMesh(std::string name);
+		~SubMesh();
+
+		std::string name; // as ID
+		i32 id;
+		u32 vertex_start;
+		u32 vertex_count;
+		u32 face_start;
+		u32 face_count;
+	};
+}
+
+#endif // _JOJ_SUBMESH_H
---
+++ joj/src/resources/submesh.cpp
@@ -0,0 +1,19 @@
+#include "resources/submesh.h"
+
+joj::SubMesh::SubMesh()
+	: name{ "" }, id{ -1 },
+	vertex_start{ 0 }, vertex_count{ 0 },
+	face_start{ 0 }, face_count{ 0 }
+{
+}
+
+joj::SubMesh::SubMesh(std::string name)
+	: name{ name }, id{ -1 },
+	vertex_start{ 0 }, vertex_count{ 0 },
+	face_start{ 0 }, face_count{ 0 }
+{
+}
+
+joj::SubMesh::~SubMesh()
+{
+}
