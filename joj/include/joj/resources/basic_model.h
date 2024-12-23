79db795a9e41f12cc8ce514f7075856ed55e6edb
Leonardo Fadul <lsffadul@gmail.com>
2024-12-22 21:29:00 -0300

    BasicModel abstract class

---
+++ joj/include/joj/resources/basic_model.h
@@ -0,0 +1,75 @@
+#ifndef _JOJ_BASIC_MODEL_H
+#define _JOJ_BASIC_MODEL_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "renderer/renderer.h"
+#include "renderer/texture_manager.h"
+#include <string>
+#include <vector>
+#include "renderer/texture2d_data.h"
+#include "systems/material/material.h"
+#include "renderer/vertex.h"
+#include "submesh.h"
+#include "mesh.h"
+#include "math/jmath.h"
+
+namespace joj
+{
+    class JAPI BasicModel
+    {
+    public:
+        BasicModel();
+        virtual ~BasicModel();
+
+        virtual ErrorCode load_m3d(GraphicsDevice& device, CommandList& cmd_list,
+            TextureManager& tex_mgr, const std::string& model_filename,
+            const std::wstring& texture_path) = 0;
+
+        u32 get_submesh_count() const;
+
+        std::vector<Material>& get_mat();
+        std::vector<TextureData2D*>& get_diffuse_map_SRV();
+        std::vector<TextureData2D*>& get_normal_map_SRV();
+
+        Mesh* get_mesh();
+
+    protected:
+        u32 m_submesh_count;
+
+        std::vector<Material> m_mat;
+        std::vector<TextureData2D*> m_diffuse_map_SRV;
+        std::vector<TextureData2D*> m_normal_map_SRV;
+
+        // Keep CPU copies of the mesh data to read from.
+        std::vector<Vertex::PosNormalTexTan> m_vertices;
+        std::vector<u16> m_indices;
+        std::vector<SubMesh> m_submeshes;
+
+        Mesh* m_mesh;
+    };
+
+    inline u32 BasicModel::get_submesh_count() const
+    { return m_submesh_count; }
+
+    inline std::vector<Material>& BasicModel::get_mat()
+    { return m_mat; }
+
+    inline std::vector<TextureData2D*>& BasicModel::get_diffuse_map_SRV()
+    { return m_diffuse_map_SRV; }
+
+    inline std::vector<TextureData2D*>& BasicModel::get_normal_map_SRV()
+    { return m_normal_map_SRV; }
+
+    inline Mesh* BasicModel::get_mesh()
+    { return m_mesh; }
+
+    struct BasicModelInstance
+    {
+        BasicModel* model;
+        JFloat4x4 world;
+    };
+}
+
+#endif // _JOJ_BASIC_MODEL_H
---
+++ joj/src/resources/basic_model.cpp
@@ -0,0 +1,13 @@
+#include "resources/basic_model.h"
+
+#include "resources/m3d_loader.h"
+#include "jmacros.h"
+
+joj::BasicModel::BasicModel()
+    : m_submesh_count{ 0 }, m_mesh{ nullptr }
+{
+}
+
+joj::BasicModel::~BasicModel()
+{
+}
