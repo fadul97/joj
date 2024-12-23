b5902c82ee586d14ef956854ca60b4fe2f3d5b68
Leonardo Fadul <lsffadul@gmail.com>
2024-12-23 06:17:28 -0300

    D3D11BasicSkinnedModel class

---
+++ joj/include/joj/resources/d3d11/basic_skinned_model_d3d11.h
@@ -0,0 +1,27 @@
+#ifndef _JOJ_D3D11_BASIC_SKINNED_MODEL_H
+#define _JOJ_D3D11_BASIC_SKINNED_MODEL_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "resources/basic_skinned_model.h"
+
+namespace joj
+{
+    class JAPI D3D11BasicSkinnedModel : public BasicSkinnedModel
+    {
+    public:
+        D3D11BasicSkinnedModel();
+        ~D3D11BasicSkinnedModel();
+
+        ErrorCode load_m3d(GraphicsDevice& device, CommandList& cmd_list,
+            TextureManager& tex_mgr, const std::string& model_filename,
+            const std::wstring& texture_path) override;
+    };
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // _JOJ_D3D11_BASIC_SKINNED_MODEL_H
