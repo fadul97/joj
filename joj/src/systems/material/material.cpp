04f8bf6a87114b3117c96c9ac9aa839c6c293afd
Leonardo Fadul <lsffadul@gmail.com>
2024-12-24 03:09:23 -0300

    Cleanup before deleting files

--- joj/src/systems/material/material.cpp
+++ joj/src/systems/material/material.cpp
@@ -1,7 +1,9 @@
 #include "systems/material/material.h"
 
 joj::Material::Material()
-    : ambient{ 0.0f, 0.0f, 0.0f, 1.0f }, diffuse{ 0.0f, 0.0f, 0.0f, 1.0f },
-    specular{ 0.0f, 0.0f, 0.0f, 1.0f }, reflect{ 0.0f, 0.0f, 0.0f, 1.0f }
+    : ambient{ 0.0f, 0.0f, 0.0f, 1.0f },
+      diffuse{ 0.0f, 0.0f, 0.0f, 1.0f },
+     specular{ 0.0f, 0.0f, 0.0f, 1.0f },
+      reflect{ 0.0f, 0.0f, 0.0f, 1.0f }
 {
 }
