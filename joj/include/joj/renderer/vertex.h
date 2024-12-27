19b9d6dd898188bce9a7016d32171ecb470ce0fb
Leonardo Fadul <lsffadul@gmail.com>
2024-12-27 18:16:37 -0300

    Funcio compare_vertex and equal operator for PosNormalTexTan struct

--- joj/include/joj/renderer/vertex.h
+++ joj/include/joj/renderer/vertex.h
@@ -24,7 +24,39 @@ namespace joj
 			JFloat3 normal;
 			JFloat2 tex;
 			JFloat4 tangentU;
+
+			bool operator==(const PosNormalTexTan& other) const
+			{
+				return pos.x == other.pos.x &&
+					pos.y == other.pos.y &&
+					pos.z == other.pos.z &&
+					normal.x == other.normal.x &&
+					normal.y == other.normal.y &&
+					normal.z == other.normal.z &&
+					tex.x == other.tex.x &&
+					tex.y == other.tex.y &&
+					tangentU.x == other.tangentU.x &&
+					tangentU.y == other.tangentU.y &&
+					tangentU.z == other.tangentU.z &&
+					tangentU.w == other.tangentU.w;
+			}
 		};
+
+		inline b8 compare_vertex_PosNormalTexTan(const PosNormalTexTan& lhs, const PosNormalTexTan& rhs)
+		{
+			return lhs.pos.x == rhs.pos.x &&
+				lhs.pos.y == rhs.pos.y &&
+				lhs.pos.z == rhs.pos.z &&
+				lhs.normal.x == rhs.normal.x &&
+				lhs.normal.y == rhs.normal.y &&
+				lhs.normal.z == rhs.normal.z &&
+				lhs.tex.x == rhs.tex.x &&
+				lhs.tex.y == rhs.tex.y &&
+				lhs.tangentU.x == rhs.tangentU.x &&
+				lhs.tangentU.y == rhs.tangentU.y &&
+				lhs.tangentU.z == rhs.tangentU.z &&
+				lhs.tangentU.w == rhs.tangentU.w;
+		}
 
 		struct PosNormalTexTanSkinned
 		{
