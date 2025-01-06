089a15c5c82f23671a9dd3f27ab8a1fd7e50d2eb
Leonardo Fadul <lsffadul@gmail.com>
2025-01-06 14:27:44 -0300

    Widget abstract class

---
+++ include/joj/renderer/gui/widget.h
@@ -0,0 +1,49 @@
+#ifndef _JOJ_WIDGET_H
+#define _JOJ_WIDGET_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "renderer/color.h"
+#include "error_code.h"
+#include "renderer/renderer.h"
+
+namespace joj
+{
+    class JAPI IWidget
+    {
+    public:
+        IWidget();
+        IWidget(const u16 x, const u16 y, const u16 width, const u16 height);
+        IWidget(const u16 x, const u16 y, const u16 width, const u16 height, const Color color);
+        ~IWidget();
+
+        virtual ErrorCode create(GraphicsDevice& device) = 0;
+
+        virtual void draw(CommandList& cmd_list) = 0;
+
+        virtual b8 is_hovered(const i32 x, const i32 y) = 0;
+
+        virtual void update() = 0;
+
+        virtual void set_background_color(const Color color) = 0;
+        virtual void set_hovered_color(const Color color) = 0;
+
+    protected:
+        u16 m_x;
+        u16 m_y;
+        u16 m_width;
+        u16 m_height;
+        Color m_background_color;
+        Color m_hovered_color;
+
+        f32 m_normalized_x;
+        f32 m_normalized_y;
+        f32 m_normalized_width;
+        f32 m_normalized_height;
+
+        b8 m_update;
+    };
+}
+
+#endif // _JOJ_WIDGET_H
