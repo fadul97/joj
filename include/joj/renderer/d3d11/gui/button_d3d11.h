bb0387a2d505e1f7d8d74354bbc7d0c21c4fd01c
Leonardo Fadul <lsffadul@gmail.com>
2025-01-06 14:29:36 -0300

    D3D11Button

---
+++ include/joj/renderer/d3d11/gui/button_d3d11.h
@@ -0,0 +1,25 @@
+#ifndef _JOJ_D3D11_BUTTON_H
+#define _JOJ_D3D11_BUTTON_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "renderer/d3d11/gui/widget_d3d11.h"
+
+namespace joj
+{
+    class JAPI D3D11Button : public D3D11Widget
+    {
+    public:
+        D3D11Button();
+        D3D11Button(const u16 x, const u16 y, const u16 width, const u16 height);
+        D3D11Button(const u16 x, const u16 y, const u16 width, const u16 height, const Color color);
+        ~D3D11Button();
+    };
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // _JOJ_D3D11_BUTTON_H
