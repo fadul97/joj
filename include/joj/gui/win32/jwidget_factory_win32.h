f4cce87ca1ab13887d996733b16cbc5e33ce56b2
Leonardo Fadul <lsffadul@gmail.com>
2025-01-07 22:58:04 -0300

    Library jojWin32GUI

---
+++ include/joj/gui/win32/jwidget_factory_win32.h
@@ -0,0 +1,37 @@
+#ifndef _JOJ_WIN32_WIDGET_FACTORY_H
+#define _JOJ_WIN32_WIDGET_FACTORY_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "gui/jwidget_factory.h"
+#include <Windows.h>
+
+namespace joj
+{
+    struct ParentData
+    {
+        HWND parent_handle;
+        HINSTANCE instance;
+    };
+
+    class JWin32WidgetFactory : public JWidgetFactory
+    {
+    public:
+        JWin32WidgetFactory();
+        JWin32WidgetFactory(const ParentData& parent_data);
+        ~JWin32WidgetFactory() = default;
+
+        JButton* create_button(i32 x, i32 y, i32 width, i32 height,
+            const std::string& label) override;
+
+    private:
+        ParentData m_parent_data;
+    };
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // _JOJ_WIN32_WIDGET_FACTORY_H
---
+++ src/gui/win32/jwidget_factory_win32.cpp
@@ -0,0 +1,35 @@
+#include "gui/win32/jwidget_factory_win32.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "logger.h"
+#include "jmacros.h"
+
+joj::JWin32WidgetFactory::JWin32WidgetFactory()
+    : m_parent_data{ nullptr, nullptr }
+{
+}
+
+joj::JWin32WidgetFactory::JWin32WidgetFactory(const ParentData& parent_data)
+{
+    m_parent_data = parent_data;
+}
+
+joj::JButton* joj::JWin32WidgetFactory::create_button(i32 x, i32 y, i32 width,
+    i32 height, const std::string& label)
+{
+    JButton* button = new JButton(x, y, width, height, label.c_str());
+    if (!button)
+    {
+        JFATAL(ErrorCode::ERR_GUI_BUTTON_WIN32_CREATION,
+            "Failed to create button.");
+        return nullptr;
+    }
+
+    JWidgetCreationData data = { m_parent_data.parent_handle, m_parent_data.instance };
+    button->create(data);
+
+    return button;
+}
+
+#endif // JPLATFORM_WINDOWS
