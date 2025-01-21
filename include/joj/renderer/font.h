b5447e30bff8840d2dadea722f6a62cd6b846752
Leonardo Fadul <lsffadul@gmail.com>
2025-01-21 03:34:27 -0300

    Helper methods and Construtor

--- include/joj/renderer/font.h
+++ include/joj/renderer/font.h
@@ -4,9 +4,8 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
-#include "texture2d_data.h"
-
 #include <string>
+#include "renderer.h"
 
 namespace joj
 {
@@ -16,12 +15,23 @@ namespace joj
     {
     public:
         Font();
+        Font(const char* filename);
         virtual ~Font();
+
+        virtual void set_font(GraphicsDevice& device, CommandList& cmd_list, const char* filename) = 0;
 
         virtual void set_data(const FontData& data) = 0;
         virtual FontData& get_data() = 0;
 
+        virtual void read_spacing(const char* filename) = 0;
         virtual void draw_text(const std::string& text, f32 x, f32 y) = 0;
+
+    public:
+        i16 m_spacing[256];
+        b8 m_proportional;
+        i32 m_char_width;
+        u32 m_tex_width;
+        u32 m_tex_height;
     };
 }
 
--- src/renderer/font.cpp
+++ src/renderer/font.cpp
@@ -1,6 +1,14 @@
 #include "renderer/font.h"
 
 joj::Font::Font()
+    : m_proportional(true), m_char_width(0),
+    m_spacing{ 0 }, m_tex_width(0), m_tex_height(0)
+{
+}
+
+joj::Font::Font(const char* filename)
+    : m_proportional(true), m_char_width(0),
+    m_spacing{ 0 }, m_tex_width(0), m_tex_height(0)
 {
 }
 
