0a31f3701a6ff6618452617c9e9d01c921e5c93f
Leonardo Fadul <lsffadul@gmail.com>
2025-01-21 20:25:23 -0300

    Rect class and Physics shader for testing

---
+++ include/joj/systems/physics/rect.h
@@ -0,0 +1,43 @@
+#ifndef _JOJ_RECT_H
+#define _JOJ_RECT_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "geometry.h"
+
+namespace joj
+{
+    class JAPI Rect : public Geometry
+    {
+    public:
+        Rect();
+        Rect(const f32 x1, const f32 y1, const f32 x2, const f32 y2);
+        ~Rect();
+
+        f32 get_left() const;
+        f32 get_top() const;
+        f32 get_right() const;
+        f32 get_bottom() const;
+
+    private:
+        f32 m_left;
+        f32 m_top;
+        f32 m_right;
+        f32 m_bottom;
+    };
+
+    inline f32 Rect::get_left() const
+    { return m_x + m_left; }
+
+    inline f32 Rect::get_top() const
+    { return m_y + m_top; }
+
+    inline f32 Rect::get_right() const
+    { return m_x + m_right; }
+
+    inline f32 Rect::get_bottom() const
+    { return m_y + m_bottom; }
+}
+
+#endif // _JOJ_RECT_H
---
+++ samples/shaders/Physics.hlsl
@@ -0,0 +1,35 @@
+cbuffer CB2D : register(b0)
+{
+    float4x4 gWorldViewProj; // Matriz de transformação (mundo -> view -> projeção)
+};
+
+struct VS_INPUT
+{
+    float3 position : POSITION; // Posição do vértice
+    float4 color : COLOR; // Coordenadas de textura
+};
+
+struct PS_INPUT
+{
+    float4 position : SV_POSITION; // Posição projetada na tela
+    float4 color : COLOR; // Coordenadas de textura interpoladas
+};
+
+PS_INPUT VS(VS_INPUT input)
+{
+    PS_INPUT output;
+
+    // Transformar a posição do vértice
+    float4 worldPosition = float4(input.position, 1.0f);
+    output.position = mul(worldPosition, gWorldViewProj);
+
+    // Ajustar coordenadas UV com base no retângulo UV fornecido
+    output.color = input.color;
+
+    return output;
+}
+
+float4 PS(PS_INPUT input) : SV_TARGET
+{
+    return input.color;
+}
