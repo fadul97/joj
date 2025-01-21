04ff2013ca4c5a272b8bf4ca123a77d1c6defbbf
Leonardo Fadul <lsffadul@gmail.com>
2025-01-21 03:35:01 -0300

    D3D11Font class and font files

---
+++ include/joj/renderer/d3d11/font_d3d11.h
@@ -0,0 +1,47 @@
+#ifndef _JOJ_D3D11_FONT_H
+#define _JOJ_D3D11_FONT_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "renderer/font.h"
+#include "texture2d_data_d3d11.h"
+
+namespace joj
+{
+    struct FontData
+    {
+        TextureData2D texture;
+        i32 rows;
+        i32 columns;
+        i32 char_width;
+        i32 char_height;
+        i32 texture_width;
+        i32 texture_height;
+    };
+
+    class JAPI D3D11Font : public Font
+    {
+    public:
+        D3D11Font();
+        D3D11Font(const char* filename);
+        ~D3D11Font();
+
+        void set_font(GraphicsDevice& device, CommandList& cmd_list, const char* filename) override;
+
+        void set_data(const FontData& data) override;
+        FontData& get_data() override;
+
+        void read_spacing(const char* filename) override;
+        void draw_text(const std::string& text, f32 x, f32 y) override;
+
+    private:
+        FontData m_data;
+    };
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // _JOJ_D3D11_FONT_H
