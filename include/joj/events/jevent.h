41d968a9fdadb6c9e80dabc38e1f218399b374b0
Leonardo Fadul <lsffadul@gmail.com>
2025-02-09 02:49:26 -0300

    Struct JEvent and children

---
+++ include/joj/events/jevent.h
@@ -0,0 +1,77 @@
+#ifndef _JOJ_EVENT_H
+#define _JOJ_EVENT_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "core/defines.h"
+
+#include "event_type.h"
+#include "platform/keys.h"
+
+namespace joj
+{
+    struct JEvent
+    {
+        EventType type;
+
+        JEvent();
+        virtual ~JEvent();
+    };
+
+    struct KeyPressedEvent : public JEvent
+    {
+        u32 key;
+
+        KeyPressedEvent(const u32 key);
+        ~KeyPressedEvent() = default;
+    };
+
+    struct KeyReleasedEvent : public JEvent
+    {
+        u32 key;
+
+        KeyReleasedEvent(const u32 key);
+        ~KeyReleasedEvent() = default;
+    };
+
+    struct MouseClickedEvent : public JEvent
+    {
+        Buttons button;
+
+        MouseClickedEvent(const Buttons button);
+        ~MouseClickedEvent() = default;
+    };
+
+    struct MouseReleasedEvent : public JEvent
+    {
+        Buttons button;
+
+        MouseReleasedEvent(const Buttons button);
+        ~MouseReleasedEvent() = default;
+    };
+
+    struct MouseMovedEvent : public JEvent
+    {
+        i32 x;
+        i32 y;
+
+        MouseMovedEvent(const i32 x, const i32 y);
+        ~MouseMovedEvent() = default;
+    };
+
+    struct WindowCloseEvent : public JEvent
+    {
+        WindowCloseEvent();
+        ~WindowCloseEvent() = default;
+    };
+
+    struct WindowResizeEvent : public JEvent
+    {
+        i32 width;
+        i32 height;
+
+        WindowResizeEvent(const i32 width, const i32 height);
+        ~WindowResizeEvent() = default;
+    };
+}
+
+#endif // _JOJ_EVENT_H
---
+++ src/events/jevent.cpp
@@ -0,0 +1,51 @@
+#include "events/jevent.h"
+
+joj::JEvent::JEvent()
+    : type(EventType::Unknown)
+{
+}
+
+joj::JEvent::~JEvent()
+{
+}
+
+joj::KeyPressedEvent::KeyPressedEvent(const u32 key)
+    : key(key)
+{
+    type = EventType::KeyPressed;
+}
+
+joj::KeyReleasedEvent::KeyReleasedEvent(const u32 key)
+    : key(key)
+{
+    type = EventType::KeyReleased;
+}
+
+joj::MouseClickedEvent::MouseClickedEvent(const Buttons button)
+    : button(button)
+{
+    type = EventType::MouseClicked;
+}
+
+joj::MouseReleasedEvent::MouseReleasedEvent(const Buttons button)
+    : button(button)
+{
+    type = EventType::MouseReleased;
+}
+
+joj::MouseMovedEvent::MouseMovedEvent(const i32 x, const i32 y)
+    : x(x), y(y)
+{
+    type = EventType::MouseMoved;
+}
+
+joj::WindowCloseEvent::WindowCloseEvent()
+{
+    type = EventType::WindowClose;
+}
+
+joj::WindowResizeEvent::WindowResizeEvent(const i32 width, const i32 height)
+    : width(width), height(height)
+{
+    type = EventType::WindowResize;
+}
