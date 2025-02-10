7dc2b3f8e068b1e917c492c23a3276d8d98be652
Leonardo Fadul <lsffadul@gmail.com>
2025-02-10 00:31:16 -0300

    Testing simple_main.jsc file

--- tests/joj_tests_jscript.cpp
+++ tests/joj_tests_jscript.cpp
@@ -22,6 +22,27 @@ JOJ_TEST(Var_Declaration)
     assert(lexer.get_errors_count() == 0);
 }
 
+JOJ_TEST(Simple_Main)
+{
+    std::cout << "\n";
+    joj::Lexer lexer("scripts/simple_main.jsc");
+    lexer.start();
+
+    joj::Token* token = lexer.scan();
+    while (token->type != joj::TokenType::END_OF_FILE)
+    {
+        std::cout << "[" << joj::tokentype_to_string(token->type) << "]: "
+            << token->lexeme << "    ";
+
+        if (token->type == joj::TokenType::SEMICOLON)
+            std::cout << "\n";
+
+        token = lexer.scan();
+    }
+
+    assert(lexer.get_errors_count() == 0);
+}
+
 int main()
 {
     joj::JTestSuite::instance().run();
