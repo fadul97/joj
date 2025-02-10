9e49dd68b6f688ae9d34d33ba7afb0848ade7e83
Leonardo Fadul <lsffadul@gmail.com>
2025-02-10 00:30:36 -0300

    SymbolTable class

---
+++ include/joj/jscript/symbol_table.h
@@ -0,0 +1,29 @@
+#ifndef _JOJ_SYMBOL_TABLE_H
+#define _JOJ_SYMBOL_TABLE_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "core/defines.h"
+
+#include <unordered_map>
+#include <string>
+#include "token.h"
+
+namespace joj
+{
+    class JAPI SymbolTable
+    {
+    public:
+        SymbolTable();
+        SymbolTable(SymbolTable* st);
+        ~SymbolTable();
+
+        b8 insert(std::string s, Token token);
+        Token* find(std::string s);
+
+    private:
+        std::unordered_map<std::string, Token> table;
+        SymbolTable* prev;
+    };
+}
+
+#endif // _JOJ_SYMBOL_TABLE_H
---
+++ src/jscript/symbol_table.cpp
@@ -0,0 +1,34 @@
+#include "jscript/symbol_table.h"
+
+joj::SymbolTable::SymbolTable()
+    : table{ std::unordered_map<std::string, Token>{} }, prev{ nullptr }
+{
+}
+
+joj::SymbolTable::SymbolTable(SymbolTable* st)
+    : table{ std::unordered_map<std::string, Token>{} }, prev{ st }
+{
+}
+
+joj::SymbolTable::~SymbolTable()
+{
+}
+
+
+b8 joj::SymbolTable::insert(std::string s, Token token)
+{
+    const auto& [pos, success] = table.insert({ s, token });
+    return success;
+}
+
+joj::Token* joj::SymbolTable::find(std::string s)
+{
+    for (SymbolTable* st = this; st != nullptr; st = st->prev)
+    {
+        auto found = st->table.find(s);
+        if (found != st->table.end())
+            return &found->second;
+    }
+
+    return nullptr;
+}
