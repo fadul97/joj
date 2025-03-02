5ea581f1f2d5894d527d47acc091aaeb8b1a1652
Leonardo Fadul <lsffadul@gmail.com>
2025-03-02 19:09:48 -0300

    Drawing button and slider

--- samples/sandbox/gui_test.cpp
+++ samples/sandbox/gui_test.cpp
@@ -23,6 +23,12 @@ GUITest::~GUITest()
 
 }
 
+void hex_to_f32_RGB(u32 hexColor, f32& r, f32& g, f32& b) {
+    r = ((hexColor >> 16) & 0xFF) / 255.0f;
+    g = ((hexColor >> 8) & 0xFF) / 255.0f;
+    b = (hexColor & 0xFF) / 255.0f;
+}
+
 void GUITest::setup_quad_buffers_and_shaders()
 {
     m_shader.compile_vertex_shader_from_file(
@@ -87,10 +93,292 @@ void GUITest::init()
     setup_quad_buffers_and_shaders();
 }
 
+// Check whether current mouse position is within a rectangle
+i32 GUITest::regionhit(i32 x, i32 y, i32 w, i32 h)
+{
+    if (uistate.mousex < x ||
+        uistate.mousey < y ||
+        uistate.mousex >= x + w ||
+        uistate.mousey >= y + h)
+        return 0;
+    return 1;
+}
+
+i32 GUITest::button(i32 id, i32 x, i32 y, const f32 r, const f32 g, const f32 b, const f32 a)
+{
+    // Check whether the button should be hot
+    if (regionhit(x, y, 64, 48))
+    {
+        uistate.hotitem = id;
+        if (uistate.activeitem == 0 && uistate.mousedown)
+            uistate.activeitem = id;
+    }
+
+    // If no widget has keyboard focus, take it
+    if (uistate.kbditem == 0)
+        uistate.kbditem = id;
+
+    // If we have keyboard focus, show it
+    if (uistate.kbditem == id)
+        draw_rect(x - 6, y - 6, 84, 68, 1.0f, 0.0f, 0.0f, 1.0f);
+
+    // Render button 
+    draw_rect(x + 8, y + 8, 64, 48, 0.0f, 0.0f, 0.0f, 1.0f);
+    if (uistate.hotitem == id)
+    {
+        if (uistate.activeitem == id)
+        {
+            // Button is both 'hot' and 'active'
+            draw_rect(x + 2, y + 2, 64, 48, r, g, b, 1.0f);
+        }
+        else
+        {
+            // Button is merely 'hot'
+            draw_rect(x, y, 64, 48, r, g, b, a);
+        }
+    }
+    else
+    {
+        // button is not hot, but it may be active    
+        draw_rect(x, y, 64, 48, r, g, b, a);
+    }
+
+    // If we have keyboard focus, we'll need to process the keys
+    if (uistate.kbditem == id)
+    {
+        switch (uistate.keyentered)
+        {
+        case joj::KEY_TAB:
+            // If tab is pressed, lose keyboard focus.
+            // Next widget will grab the focus.
+            uistate.kbditem = 0;
+            // If shift was also pressed, we want to move focus
+            // to the previous widget instead.
+            if (uistate.keymod)
+                uistate.kbditem = uistate.lastwidget;
+            // Also clear the key so that next widget
+            // won't process it
+            uistate.keyentered = 0;
+            break;
+        case joj::KEY_ENTER:
+            // Had keyboard focus, received return,
+            // so we'll act as if we were clicked.
+            return 1;
+        }
+    }
+
+    uistate.lastwidget = id;
+
+    // If button is hot and active, but mouse button is not
+    // down, the user must have clicked the button.
+    if (uistate.mousedown == 0 &&
+        uistate.hotitem == id &&
+        uistate.activeitem == id)
+        return 1;
+
+    // Otherwise, no clicky.
+    return 0;
+}
+
+// Simple scroll bar IMGUI widget
+i32 GUITest::slider(i32 id, i32 x, i32 y, i32 max, i32& value)
+{
+    // Calculate mouse cursor's relative y offset
+    i32 ypos = ((256 - 16) * value) / max;
+
+    f32 r = 0.0f;
+    f32 g = 0.0f;
+    f32 b = 0.0f;
+
+    // Check for hotness
+    if (regionhit(x + 8, y + 8, 16, 255))
+    {
+        uistate.hotitem = id;
+        if (uistate.activeitem == 0 && uistate.mousedown)
+            uistate.activeitem = id;
+    }
+
+    // If no widget has keyboard focus, take it
+    if (uistate.kbditem == 0)
+        uistate.kbditem = id;
+
+    // If we have keyboard focus, show it
+    if (uistate.kbditem == id)
+        draw_rect(x - 4, y - 4, 40, 280, 1.0f, 0.0f, 0.0f, 1.0f);
+
+    // Render the scrollbar
+    draw_rect(x, y, 32, 256 + 16, 0.46f, 0.46f, 0.46f, 1.0f);
+
+    if (uistate.activeitem == id || uistate.hotitem == id)
+    {
+        draw_rect(x + 8, y + 8 + ypos, 16, 16, 1.0f, 1.0f, 1.0f, 1.0f);
+    }
+    else
+    {
+        hex_to_f32_RGB(0xaaaaaa, r, g, b);
+        draw_rect(x + 8, y + 8 + ypos, 16, 16, r, g, b, 1.0f);
+    }
+
+    // If we have keyboard focus, we'll need to process the keys
+    if (uistate.kbditem == id)
+    {
+        switch (uistate.keyentered)
+        {
+        case joj::KEY_TAB:
+            // If tab is pressed, lose keyboard focus.
+            // Next widget will grab the focus.
+            uistate.kbditem = 0;
+            // If shift was also pressed, we want to move focus
+            // to the previous widget instead.
+            if (uistate.keymod)
+                uistate.kbditem = uistate.lastwidget;
+            // Also clear the key so that next widget
+            // won't process it
+            uistate.keyentered = 0;
+            break;
+        case joj::KEY_UP:
+            // Slide slider up (if not at zero)
+            if (value > 0)
+            {
+                value--;
+                return 1;
+            }
+            break;
+        case joj::KEY_DOWN:
+            // Slide slider down (if not at max)
+            if (value < max)
+            {
+                value++;
+                return 1;
+            }
+            break;
+        }
+    }
+
+    uistate.lastwidget = id;
+
+    // Update widget value
+    if (uistate.activeitem == id)
+    {
+        i32 mousepos = uistate.mousey - (y + 8);
+        if (mousepos < 0) mousepos = 0;
+        if (mousepos > 255) mousepos = 255;
+        i32 v = (mousepos * max) / 255;
+        if (v != value)
+        {
+            value = v;
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+// Prepare for IMGUI code
+void GUITest::imgui_prepare()
+{
+    uistate.hotitem = 0;
+}
+
+void GUITest::imgui_finish()
+{
+    if (uistate.mousedown == 0)
+    {
+        uistate.activeitem = 0;
+    }
+    else
+    {
+        if (uistate.activeitem == 0)
+            uistate.activeitem = -1;
+    }
+    // If no widget grabbed tab, clear focus
+    if (uistate.keyentered == joj::KEY_TAB)
+        uistate.kbditem = 0;
+
+    // Clear the entered key
+    uistate.keyentered = 0;
+}
+
+void GUITest::render(const f32 dt)
+{
+    static i32 bgcolor = 0x77;
+
+    static f32 r = 0.5f;
+    static f32 g = 0.5f;
+    static f32 b = 0.5f;
+    // draw_rect(0, 0, 640, 480, r, g, b, 1.0f);
+
+    imgui_prepare();
+
+    button(GEN_ID, 50, 50, r, g, b, 1.0f);
+
+    button(GEN_ID, 150, 50, r, g, b, 1.0f);
+
+    if (button(GEN_ID, 50, 150, r, g, b, 1.0f))
+    {
+        r = 1.0f;
+        g = 1.0f;
+        b = 0.0f;
+    }
+
+    if (button(GEN_ID, 150, 150, r, g, b, 1.0f))
+        joj::Engine::close();
+
+    i32 slidervalue = bgcolor & 0xff;
+    if (slider(GEN_ID, 500, 40, 255, slidervalue))
+    {
+        bgcolor = (bgcolor & 0xffff00) | slidervalue;
+    }
+
+    slidervalue = ((bgcolor >> 10) & 0x3f);
+    if (slider(GEN_ID, 550, 40, 63, slidervalue))
+    {
+        bgcolor = (bgcolor & 0xff00ff) | (slidervalue << 10);
+    }
+
+    slidervalue = ((bgcolor >> 20) & 0xf);
+    if (slider(GEN_ID, 600, 40, 15, slidervalue))
+    {
+        bgcolor = (bgcolor & 0x00ffff) | (slidervalue << 20);
+    }
+
+    imgui_finish();
+}
+
 void GUITest::update(const f32 dt)
 {
     if (m_input->is_key_pressed(joj::KEY_ESCAPE))
         joj::Engine::close();
+
+    {
+        if (m_input->is_button_down(joj::BUTTON_LEFT))
+            uistate.mousedown = 1;
+        if (m_input->is_button_up(joj::BUTTON_LEFT))
+            uistate.mousedown = 0;
+
+        uistate.mousex = m_input->get_xmouse();
+        uistate.mousey = m_input->get_ymouse();
+
+        if (m_input->is_key_pressed(joj::KEY_TAB))
+            uistate.keyentered = joj::KEY_TAB;
+
+        if (m_input->is_key_pressed(joj::KEY_ENTER))
+            uistate.keyentered = joj::KEY_ENTER;
+
+        if (m_input->is_key_up(joj::KEY_SHIFT))
+            uistate.keymod = 0;
+
+        if (m_input->is_key_pressed(joj::KEY_SHIFT))
+            uistate.keymod = joj::KEY_SHIFT;
+
+        if (m_input->is_key_pressed(joj::KEY_UP))
+            uistate.keyentered = joj::KEY_UP;
+
+        if (m_input->is_key_pressed(joj::KEY_DOWN))
+            uistate.keyentered = joj::KEY_DOWN;
+    }
+
+    m_dt = dt;
 }
 
 void GUITest::draw_quad()
@@ -113,7 +401,7 @@ void GUITest::draw_quad()
     m_renderer->get_cmd_list().device_context->DrawIndexed(6, 0, 0);
 }
 
-void GUITest::draw_rect(const i32 x, const i32 y, const i32 w, const i32 h, const i32 r, const i32 g, const i32 b, const i32 a)
+void GUITest::draw_rect(const i32 x, const i32 y, const i32 w, const i32 h, const f32 r, const f32 g, const f32 b, const f32 a)
 {
     constexpr i32 screen_width = 800;
     constexpr i32 screen_height = 600;
@@ -143,7 +431,7 @@ void GUITest::draw_rect(const i32 x, const i32 y, const i32 w, const i32 h, cons
     m_vb.bind(m_renderer->get_cmd_list(), 0, 1, &stride, &offset);
     m_ib.bind(m_renderer->get_cmd_list(), joj::DataFormat::R32_UINT, offset);
 
-    ColorBuffer rectColor = { 1.0f, 1.0f, 0.0f, 1.0f };
+    ColorBuffer rectColor = { r, g, b, a };
     m_cb.bind_to_vertex_shader(m_renderer->get_cmd_list(), 0, 1);
     m_cb.bind_to_pixel_shader(m_renderer->get_cmd_list(), 0, 1);
     m_cb.update(m_renderer->get_cmd_list(), rectColor);
@@ -155,7 +443,10 @@ void GUITest::draw()
 {
     m_renderer->clear();
 
-    draw_rect(100, 100, 150, 100, 1.0f, 0.0f, 0.0f, 1.0f);
+    m_renderer->disable_depth_test();
+    render(m_dt);
+    // draw_rect(uistate.mousex - 32, uistate.mousey - 24, 150, 100, 1.0f, 0.0f, 0.0f, 1.0f);
+    m_renderer->enable_depth_test();
 
     m_renderer->swap_buffers();
 }
--- samples/sandbox/gui_test.h
+++ samples/sandbox/gui_test.h
@@ -12,6 +12,30 @@
 
 // ----------------------------------------------------------------------------
 
+#ifdef IMGUI_SRC_ID
+#define GEN_ID ((IMGUI_SRC_ID) + (__LINE__))
+#else
+#define GEN_ID (__LINE__)
+#endif
+
+struct UIState
+{
+    i32 mousex;
+    i32 mousey;
+    i32 mousedown;
+
+    i32 hotitem;
+    i32 activeitem;
+
+    i32 kbditem;
+    i32 keyentered;
+    i32 keymod;
+
+    i32 lastwidget;
+};
+
+// ----------------------------------------------------------------------------
+
 class GUITest : public joj::App
 {
 public:
@@ -25,7 +49,15 @@ public:
 
     void setup_quad_buffers_and_shaders();
     void draw_quad();
-    void draw_rect(const i32 x, const i32 y, const i32 w, const i32 h, const i32 r, const i32 g, const i32 b, const i32 a);
+    void draw_rect(const i32 x, const i32 y, const i32 w, const i32 h, const f32 r, const f32 g, const f32 b, const f32 a);
+
+    i32 regionhit(i32 x, i32 y, i32 w, i32 h);
+    i32 button(i32 id, i32 x, i32 y, const f32 r, const f32 g, const f32 b, const f32 a);
+    i32 slider(i32 id, i32 x, i32 y, i32 max, i32& value);
+    void imgui_prepare();
+    void imgui_finish();
+    void render(const f32 dt);
+    f32 m_dt = 0.0f;
 
     // ---------------------------------------------------
 
@@ -34,6 +66,8 @@ public:
     joj::D3D11VertexBuffer m_vb;
     joj::D3D11IndexBuffer m_ib;
     joj::D3D11ConstantBuffer m_cb;
+
+    UIState uistate = { 0,0,0,0,0,0,0,0,0 };
 };
 
 #endif // _JOJ_GUI_TEST_H
