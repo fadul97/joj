1cb3d0c9255c348c3221da8577672a73b3651e0e
Leonardo Fadul <lsffadul@gmail.com>
2025-03-25 01:28:40 -0300

    Library jojUtils and WIP Namespace JsonParser

---
+++ include/joj/utils/json_parser.h
@@ -0,0 +1,14 @@
+#ifndef _JOJ_JSON_PARSER_H
+#define _JOJ_JSON_PARSER_H
+
+#include "joj/core/defines.h"
+
+namespace joj
+{
+    namespace JsonParser
+    {
+        JOJ_API void hello_world();
+    }
+}
+
+#endif // _JOJ_JSON_PARSER_H
---
+++ src/utils/json_parser.cpp
@@ -0,0 +1,8 @@
+#include "joj/utils/json_parser.h"
+
+#include <iostream>
+
+void joj::JsonParser::hello_world()
+{
+    std::cout << "Hello, Json Parser!" << std::endl;
+}
