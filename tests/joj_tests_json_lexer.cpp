8816fbe6f3db7c107f4a5809a32a659e4b4e212d
Leonardo Fadul <lsffadul@gmail.com>
2025-03-25 13:28:28 -0300

    Testing JsonParser

--- tests/joj_tests_json_lexer.cpp
+++ tests/joj_tests_json_lexer.cpp
@@ -1,23 +1,66 @@
 #include "joj/test/jtest_macros.h"
 
 #include "joj/utils/json_lexer.h"
+#include "joj/utils/json_parser.h"
 #include <string>
 #include "joj/core/logger.h"
 #include <print>
 
-JOJ_TEST(Parser_Var_Declaration)
+JOJ_TEST(JSON_TEST_1)
 {
-    std::string json = R"({"name": "Cube", "vertices": [0, 1, 2]})";
+    std::string json = R"(
+        {
+            "name": "Cube",
+            "vertices": [0, 1, 2]
+        }
+    )";
     joj::JsonLexer lexer(json);
 
+    std::printf("\n");
     joj::JsonToken token;
     while ((token = lexer.next_token()).type != joj::JsonTokenType::EndOfFile)
     {
         // Print token
-        std::printf("\nToken: %s", token.value.c_str());
+        std::printf("Token: %s\n", token.value.c_str());
         assert(token.type != joj::JsonTokenType::Error);
     }
 }
+
+JOJ_TEST(JSON_TEST_2)
+{
+    std::string json = R"(
+        {
+            "name": "Cube",
+            "vertices": [0, 1, 2],
+            "visible": true
+        }
+    )";
+    joj::JsonLexer lexer(json);
+
+    std::printf("\n");
+    joj::JsonToken token;
+    while ((token = lexer.next_token()).type != joj::JsonTokenType::EndOfFile)
+    {
+        // Print token
+        std::printf("Token: %s\n", token.value.c_str());
+        assert(token.type != joj::JsonTokenType::Error);
+    }
+}
+
+JOJ_TEST(JSON_TEST_3)
+{
+    std::string json = R"(
+        {
+            "name": "Cube",
+            "vertices": [0, 1, 2],
+            "visible": true
+        }
+    )";
+
+    joj::JsonParser parser(json);
+    joj::JsonValue result = parser.parse();
+}
+
 
 int main() {
     joj::JTestSuite::instance().run();
