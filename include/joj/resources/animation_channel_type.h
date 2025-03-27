fe22926c95a57ceb437fc8336fa91166ab18bb3f
Leonardo Fadul <lsffadul@gmail.com>
2025-03-26 22:28:42 -0300

    Enum class AnimationChannelType

---
+++ include/joj/resources/animation_channel_type.h
@@ -0,0 +1,15 @@
+#ifndef _JOJ_ANIMATION_CHANNEL_TYPE_H
+#define _JOJ_ANIMATION_CHANNEL_TYPE_H
+
+namespace joj
+{
+    enum class AnimationChannelType
+    {
+        TRANSLATION,
+        ROTATION,
+        SCALE,
+        UNKNOWN
+    };
+}
+
+#endif // _JOJ_ANIMATION_CHANNEL_TYPE_H
