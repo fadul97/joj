b3c0aeca01f34fbf87d2ec27641815257ce77883
Leonardo Fadul <lsffadul@gmail.com>
2025-03-26 23:15:11 -0300

    Enum class ComponentType and Enum class DataType

---
+++ include/joj/resources/component_type.h
@@ -0,0 +1,16 @@
+#ifndef _JOJ_COMPONENT_TYPE_H
+#define _JOJ_COMPONENT_TYPE_H
+
+namespace joj
+{
+    enum class ComponentType
+    {
+        U16,
+        U32,
+        F32,
+        F64,
+        UNKNOWN
+    };
+}
+
+#endif // _JOJ_COMPONENT_TYPE_H
---
+++ include/joj/resources/data_type.h
@@ -0,0 +1,16 @@
+#ifndef _JOJ_DATA_TYPE_H
+#define _JOJ_DATA_TYPE_H
+
+namespace joj
+{
+    enum class DataType
+    {
+        VEC2,
+        VEC3,
+        VEC4,
+        SCALAR,
+        UNKNOWN
+    };
+}
+
+#endif // _JOJ_DATA_TYPE_H
