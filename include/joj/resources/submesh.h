bb4c464e44576d7539ca5c45e7da15eb2a533832
Leonardo Fadul <lsffadul@gmail.com>
2025-04-01 20:23:57 -0300

    Simple Struct Submesh

---
+++ include/joj/resources/submesh.h
@@ -0,0 +1,23 @@
+#ifndef _JOJ_SUBMESH_H
+#define _JOJ_SUBMESH_H
+
+#include "joj/core/defines.h"
+
+#include <string>
+
+namespace joj
+{
+    struct JOJ_API Submesh
+    {
+        Submesh();
+        ~Submesh();
+
+        std::string name;
+        i32 vertex_start;
+        i32 vertex_count;
+        i32 index_start;
+        i32 index_count;
+    };
+}
+
+#endif // _JOJ_SUBMESH_H
---
+++ src/resources/submesh.cpp
@@ -0,0 +1,14 @@
+#include "joj/resources/submesh.h"
+
+joj::Submesh::Submesh()
+    : name("")
+    , vertex_start(-1)
+    , vertex_count(-1)
+    , index_start(-1)
+    , index_count(-1)
+{
+}
+
+joj::Submesh::~Submesh()
+{
+}
