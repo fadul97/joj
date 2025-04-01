158c77f3e37eba521123ee22c37e5ac207ec19da
Leonardo Fadul <lsffadul@gmail.com>
2025-04-01 00:04:35 -0300

    Function to_str for AnimmationChannelType

--- include/joj/resources/animation_channel_type.h
+++ include/joj/resources/animation_channel_type.h
@@ -1,6 +1,8 @@
 #ifndef _JOJ_ANIMATION_CHANNEL_TYPE_H
 #define _JOJ_ANIMATION_CHANNEL_TYPE_H
 
+#include "joj/core/defines.h"
+
 namespace joj
 {
     enum class AnimationChannelType
@@ -8,8 +10,11 @@ namespace joj
         TRANSLATION,
         ROTATION,
         SCALE,
+        WEIGHTS,
         UNKNOWN
     };
+
+    JOJ_API const char* animation_channel_type_to_string(const AnimationChannelType type);
 }
 
 #endif // _JOJ_ANIMATION_CHANNEL_TYPE_H
