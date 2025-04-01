3338e25141ba03d02754f6858c9331d42a824aca
Leonardo Fadul <lsffadul@gmail.com>
2025-04-01 00:05:35 -0300

    Struct GLTFAnimation

---
+++ include/joj/resources/gltf/gltf_animation.h
@@ -0,0 +1,21 @@
+#ifndef _JOJ_GLTF_ANIMATION_H
+#define _JOJ_GLTF_ANIMATION_H
+
+#include "joj/core/defines.h"
+
+#include <string>
+#include <vector>
+#include "gltf_animation_channel.h"
+#include "gltf_sampler.h"
+
+namespace joj
+{
+    struct JOJ_API GLTFAnimation
+    {
+        std::string name;
+        std::vector<GLTFAnimationChannel> channels;
+        std::vector<GLTFSampler> samplers;
+    };
+}
+
+#endif // _JOJ_GLTF_ANIMATION_H
