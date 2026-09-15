c62932a51632dea819915fb93cc520a4d08bedef
Leonardo Fadul <lsffadul@gmail.com>
2026-09-15 20:24:27 -0300

    joj Library

---
+++ include/joj/joj.hpp
@@ -0,0 +1,10 @@
+#ifndef _JOJ_HPP
+#define _JOJ_HPP
+
+namespace joj {
+
+void print();
+
+} // namespace joj
+
+#endif // _JOJ_HPP
---
+++ src/joj.cpp
@@ -0,0 +1,12 @@
+#include "joj/joj.hpp"
+
+#include <stdio.h>
+
+namespace joj {
+
+void print()
+{
+    printf("Hello, JOJ!\n");
+}
+
+} // namespace joj
