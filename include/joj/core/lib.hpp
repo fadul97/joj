545119be6acabdf464e666db7d0fb0ca1369c28a
Leonardo Fadul <lsffadul@gmail.com>
2026-09-15 21:00:45 -0300

    WIP(joj): jojCore Library

---
+++ include/joj/core/lib.hpp
@@ -0,0 +1,10 @@
+#ifndef _JOJ_LIB_HPP
+#define _JOJ_LIB_HPP
+
+namespace joj {
+
+void abort();
+
+} // namespace joj
+
+#endif // _JOJ_LIB_HPP
---
+++ src/core/lib.cpp
@@ -0,0 +1,12 @@
+#include "joj/core/lib.hpp"
+
+#include <cstdlib>
+
+namespace joj {
+
+void abort()
+{
+    std::abort();
+}
+
+} // namespace joj
