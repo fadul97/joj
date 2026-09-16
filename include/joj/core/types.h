b4960714ef37bc9844621d4e875cc7dd4ac2d3f3
Leonardo Fadul <lsffadul@gmail.com>
2026-09-16 19:17:15 -0300

    WIP(joj): types used throught the engine

---
+++ include/joj/core/types.h
@@ -0,0 +1,38 @@
+#ifndef _JOJ_TYPES_H
+#define _JOJ_TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float f32;
+typedef double f64;
+
+typedef bool b8;
+typedef uint32_t b32;
+
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
+
+static_assert(sizeof(f32) == 4);
+static_assert(sizeof(f64) == 8);
+
+static_assert(sizeof(b8) == 1);
+static_assert(sizeof(b32) == 4);
+
+#endif // _JOJ_TYPES_H
