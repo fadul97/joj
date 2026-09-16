3af870d69ca4720647f32f1d3213bae9341c49e2
Leonardo Fadul <lsffadul@gmail.com>
2026-09-16 19:19:41 -0300

    WIP(joj): use types from engine

--- src/joj.cpp
+++ src/joj.cpp
@@ -7,14 +7,22 @@
 #include <unistd.h>
 
 // XCB Includes
+#include <vulkan/vulkan_core.h>
 #include <xcb/xcb.h>
+
+// Vulkan Includes
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_xcb.h>
 
 // joj Includes
 #include "joj/core/typedefs.h"
+#include "joj/core/types.h"
+
+#define VK_FAILED(result) ((result) != VK_SUCCESS)
 
 namespace joj {
 
-int main(MainArgs const& args)
+i32 main(MainArgs const& args)
 {
     if (args.argv.capacity() > 1)
     {
@@ -23,14 +31,18 @@ int main(MainArgs const& args)
     }
 
     // Debug
-    for (unsigned int i = 0; i < args.argv.capacity(); ++i)
+    for (u32 i = 0; i < args.argv.capacity(); ++i)
     {
         printf("Argv[%d]: `%s`\n", i, args.argv[i].c_str());
     }
 
-    int screen_num = 0;
+    // ------------------------------------------------------------------------
+    // Create Window
+    // ------------------------------------------------------------------------
+
+    i32 screen_num = 0;
     xcb_connection_t* connection = xcb_connect(nullptr, &screen_num);
-    int ret = xcb_connection_has_error(connection);
+    i32 ret = xcb_connection_has_error(connection);
     if (ret != 0)
     {
         printf("Failed to connect to server.\n");
@@ -40,7 +52,7 @@ int main(MainArgs const& args)
     xcb_setup_t const* setup = xcb_get_setup(connection);
     xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
 
-    for (int i = 0; i < screen_num; ++i)
+    for (i32 i = 0; i < screen_num; ++i)
     {
         xcb_screen_next(&iter);
     }
@@ -55,8 +67,8 @@ int main(MainArgs const& args)
 
     printf("\tWidth - 1920: %" PRIu16 "\n", screen->width_in_pixels - 1920);
 
-    unsigned int masks = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-    unsigned int values[3] = { screen->white_pixel, XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS, 0 };
+    u32 masks = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+    u32 values[3] = { screen->white_pixel, XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS, 0 };
 
     xcb_window_t window = xcb_generate_id(connection);
     if (window < 0)
@@ -76,8 +88,8 @@ int main(MainArgs const& args)
         screen->root_visual,
         masks, values);
 
-    unsigned int gfx_mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
-    unsigned int gfx_values[3] = { screen->black_pixel, 0, 0 };
+    u32 gfx_mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
+    u32 gfx_values[3] = { screen->black_pixel, 0, 0 };
 
     xcb_gcontext_t graphics_context = xcb_generate_id(connection);
     xcb_create_gc(connection, graphics_context, window, gfx_mask, gfx_values);
@@ -85,9 +97,67 @@ int main(MainArgs const& args)
     xcb_map_window(connection, window);
     xcb_flush(connection);
 
+    // ------------------------------------------------------------------------
+    // Create Vulkan Instance
+    // ------------------------------------------------------------------------
+
+    // Describe application
+    VkApplicationInfo const app_info{
+        // ID of the struct
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        // Struct extension
+        .pNext = nullptr,
+        // Name of the application
+        .pApplicationName = "Hello, Vulkan Instance",
+        // Version of the application
+        .applicationVersion = VK_MAKE_VERSION(0, 1, 0),
+        // Name of the engine
+        .pEngineName = "JOJ",
+        // Version of the engine
+        .engineVersion = VK_MAKE_VERSION(0, 1, 0),
+        // Vulkan API version requested
+        .apiVersion = VK_API_VERSION_1_3,
+    };
+
+    char const* const extensions[]{
+        VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+        VK_KHR_SURFACE_EXTENSION_NAME,
+        VK_KHR_XCB_SURFACE_EXTENSION_NAME
+    };
+
+    // Describe instance
+    VkInstanceCreateInfo const instance_ci{
+        // IF of the struct
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        // Struct extension
+        .pNext = nullptr,
+        // Behaviour of this instance
+        .flags = 0,
+        .pApplicationInfo = &app_info,
+        // Number of global layers to enable
+        .enabledLayerCount = 0,
+        // Pointer to layer names
+        .ppEnabledLayerNames = extensions,
+        // Number of global extensions to enblae
+        .enabledExtensionCount = 0,
+        // Pointer to extension names
+        .ppEnabledExtensionNames = nullptr,
+    };
+
+    VkAllocationCallbacks* m_allocator{ nullptr };
+
+    VkInstance m_instance{ nullptr };
+    VkResult result = vkCreateInstance(&instance_ci, m_allocator, &m_instance);
+    if VK_FAILED (result)
+    {
+        printf("[ERROR]: Failed to create Vulkan Instance.\n");
+        xcb_disconnect(connection);
+        return -1;
+    }
+
     xcb_rectangle_t r = { 20, 20, 60, 60 };
 
-    bool running = true;
+    b8 running = true;
     while (running)
     {
         xcb_generic_event_t* e = xcb_wait_for_event(connection);
@@ -115,6 +185,8 @@ int main(MainArgs const& args)
             free(e);
         }
     }
+
+    vkDestroyInstance(m_instance, m_allocator);
 
     xcb_disconnect(connection);
 
