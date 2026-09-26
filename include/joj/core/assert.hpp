64116da9c02d5084afb5543e18eb3e8cd2cc9ec8
Leonardo Fadul <lsffadul@gmail.com>
2026-09-26 15:53:18 -0300

    JOJ_FAILED macro

--- include/joj/core/assert.hpp
+++ include/joj/core/assert.hpp
@@ -1,6 +1,7 @@
 #ifndef _JOJ_ASSERT_HPP
 #define _JOJ_ASSERT_HPP
 
+#include "joj/core/error/error_code.hpp"
 #include "joj/core/lib.hpp"
 
 #define JOJ_ASSERT(cond)  \
@@ -22,5 +23,7 @@
 #else
 #define JOJ_ASSERT_DEBUG(cond)
 #endif
+
+#define JOJ_FAILED(err_code) ((err_code) != (joj::ErrorCode::OK))
 
 #endif // _JOJ_ASSERT_HPP
